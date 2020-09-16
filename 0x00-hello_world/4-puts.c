f0f69386eceb978aaf073d7c83cf55cf641f3c9d
JDParnell <justindparnell@yahoo.com>
2020-09-16 08:15:22 +0000

    4-puts.c v1.0

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char tmp [] = "\"Programming is like building a multilingual puzzle";
+	puts(tmp);
+	return 0;
+}
