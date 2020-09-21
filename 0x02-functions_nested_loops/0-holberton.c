b65fddf6f56722a8fde6c2cac96e61527d797c40
JDParnell <justindparnell@yahoo.com>
2020-09-21 21:10:19 +0000

    0-holberton.c v2.0

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,17 @@
+/**
+ *main - prints holberton
+ *
+ *Return: 0 always success
+ *
+ */
+
+int main(void)
+{
+	char tmp[] = "Holberton";
+	int i;
+
+	for (i = 0; i < 9; i++)
+		_putchar(tmp[i]);
+	_putchar('\n');
+	return (0);
+}
