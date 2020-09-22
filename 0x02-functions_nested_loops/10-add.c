78408e4177b5a988ffaf9d0c57ff548dcbf1c23c
JDParnell <justindparnell@yahoo.com>
2020-09-22 05:09:31 +0000

    10-add.c v1.0

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ *add - returns a + b
+ *@a: first number being added
+ *@b: second number being added
+ *
+ *Return: a + b
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
