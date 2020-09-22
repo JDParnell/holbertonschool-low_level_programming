cdbd6e5bb3455f0ee99ae5631dbf539b7383f70f
JDParnell <justindparnell@yahoo.com>
2020-09-22 04:51:30 +0000

    2-print_alphabet_x10.c v2.0

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,9 +1,15 @@
 #include "holberton.h"
 
+/**
+ *print_alphabet_x10 - prints a-z 10 times
+ *
+ */
+
 void print_alphabet_x10(void)
 {
 	int i;
 	int n;
+
 	for (i = 0; i <= 9; i++)
 	{
 		for (n = 'a'; n <= 'z'; n++)
