dd8cbc6e026c0bf3e9762266667376d1fe0396cf
JDParnell <justindparnell@yahoo.com>
2020-09-22 05:11:49 +0000

    11-print_to_98.c v1.0

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ *print_to_98 - prints all numbers between n and 98
+ *@n: the number that we are starting the counting from
+ */
+
+void print_to_98(int n)
+{
+	int i;
+
+	if (n < 98)
+	{
+		for (i = n; i < 98; i++)
+			printf("%d, ", i);
+	}
+	else if (n > 98)
+	{
+		for (i = n; i > 98; i--)
+			printf("%d, ", i);
+	}
+	printf("98\n");
+}
