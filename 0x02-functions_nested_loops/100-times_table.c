7ecc83ba3fd7e3d7ae2db29017158d8514564a04
JDParnell <justindparnell@yahoo.com>
2020-09-24 03:13:18 +0000

    100-times_table.c v1.0

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,44 @@
+#include "holberton.h"
+/**
+ *print_times_table - creates a times table of n
+ *@n: the number to loop up to
+ *Return: no return
+ */
+void print_times_table(int n)
+{
+	int i, j, m, x, y, z;
+
+	if (n > 0 && n < 16)
+	{
+		for (i = 0; i <= n; i++)
+		{
+			for (j = 0; j <= n; j++)
+			{
+				m = i * j;
+				x = m / 100;
+				y = ((m / 10) % 10);
+				z = m % 10;
+				m = i * (j + 1);
+				if (x != 0)
+					_putchar('0' + x);
+				if (y != 0)
+					_putchar('0' + y);
+				_putchar('0' + z);
+				if (j == n)
+				{
+					_putchar('\n');
+				}
+				else
+				{
+					_putchar(',');
+					_putchar(' ');
+					if (m / 100 == 0)
+						_putchar(' ');
+					if ((m / 10) % 10 == 0)
+						_putchar(' ');
+				}
+
+			}
+		}
+	}
+}
