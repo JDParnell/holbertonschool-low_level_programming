d46c96583ebbe05d7ffa920bd6d7175211541e96
JDParnell <justindparnell@yahoo.com>
2020-09-24 06:00:23 +0000

    2-mul.c v1.0

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+/**
+ *mul - multiplies two numbers
+ *@a; first number being multiplied
+ *@b: second number being multiplied
+ *Return: a * b
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
