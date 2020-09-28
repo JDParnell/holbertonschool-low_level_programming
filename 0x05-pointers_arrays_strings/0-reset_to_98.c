5dfb0f7c6cd6bed098554913bec6b66c00d7e399
Selidex <justindparnell@yahoo.com>
2020-09-28 06:24:57 +0000

    0-reset_to_98.c v1.0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+ *reset_to_98 - takes a pointer and resets the value to 98
+ *@n: the pointer being passed
+ *Return: void so no returns
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
