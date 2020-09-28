637721d961584f1c0ba7b999f0652fe75cc9f06e
Selidex <justindparnell@yahoo.com>
2020-09-28 08:37:36 +0000

    2-strlen.c v1.0

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ *_strlen - returns the lenght of the string passed in
+ *@s: the string being passed
+ *Return: the lenght of the string
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+
+	}
+
+	return (i);
+}
