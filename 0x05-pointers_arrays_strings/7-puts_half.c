372e504d568b331837b9282b9e23fe9d73616e4b
Selidex <justindparnell@yahoo.com>
2020-09-29 06:40:26 +0000

    7-puts_half.c v1.0

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ *puts_half - prints the second half of a string
+ *@str: the string to be halved
+ *Return: void no return
+ */
+
+void puts_half(char *str)
+{
+	int i, n;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+	}
+	if (i % 2 == 0)
+		n = i / 2;
+	else
+		n = (i - 1) / 2;
+
+	for (; n < i; n++)
+		_putchar(str[n]);
+	_putchar('\n');
+
+}
