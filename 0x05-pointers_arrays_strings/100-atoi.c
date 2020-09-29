60b5c9af47cd8cfc39d20330e0080fdf048d8203
Selidex <justindparnell@yahoo.com>
2020-09-29 21:56:04 +0000

    100-atoi.c v1.2 revisit

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -18,13 +18,11 @@ int _atoi(char *s)
 			neg *= -1;
 		if (s[i] - 48 < 10 && s[i] - 48 >= 0)
 		{
-			ext = (ext * 10) + ((s[i] - 48) * neg);
-			if (ext / 10 == 0)
-				ext *= neg;
+			ext = (ext * 10) + (s[i] - 48);
 			if (s[i + 1] < '0' || s[i + 1] > '9')
 				break;
 		}
 	}
 
-		return (ext);
+		return (ext * neg);
 }
