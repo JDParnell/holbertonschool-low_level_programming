bff94b7ebac096789d36396d33e3d6dcfa813520
Selidex <justindparnell@yahoo.com>
2020-10-01 07:44:02 +0000

    1-strncat.c v1.0

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ *_strncat - adds one string to another, up to n or NULL
+ *@dest: the string being added to
+ *@src: the string getting added to dest
+ *@n: the maximum number of characters being added
+ *Return: the pointer to dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j;
+
+	for (i = 0; dest[i] != '\0'; i++)
+	{}
+
+	for (j = 0; j < (n - 1) && src[j] != '\0'; j++)
+	{
+		dest[i] = src[j];
+		i++;
+	}
+	dest[i] = src[j];
+	return (dest);
+}
