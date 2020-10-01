28205765985b5c65d51815f024d5b9a150bfb8db
Selidex <justindparnell@yahoo.com>
2020-10-01 20:27:46 +0000

    7-leet.c v1.0

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ *leet - converts into leet speak
+ *@s: the string being converted
+ *Return: the converted string
+ */
+
+char *leet(char *s)
+{
+	int i, j;
+	char con[] = {'4', '3', '0', '1', '7'};
+	char test[] = { 'a', 'A', 'e', 'E', 'o', 'O', 'i', 'I', 'l', 'L'};
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if (s[i] == test[j])
+				s[i] = con[j / 2];
+		}
+	}
+
+	return (s);
+}
