e21fce272574c8c6e3d505a2842898e1132ce797
Selidex <justindparnell@yahoo.com>
2020-10-01 21:51:50 +0000

    8-rot13.c v1.0

---
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ *rot13 - rotates alpha values by 13
+ *@s: the string being rotated
+ *Return: pointer to the rotated string
+ */
+
+char *rot13(char *s)
+{
+	int i, j;
+	char con[] = {"nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM"};
+	char test[] = {"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"};
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; j < 52; j++)
+		{
+			if (s[i] == test[j])
+			{
+				s[i] = con[j];
+			}
+		}
+	}
+	return (s);
+}
