bc734ed3f3faac5fcf7acbc0c667d08f3378bbad
Selidex <justindparnell@yahoo.com>
2020-10-01 22:47:20 +0000

    100-print_number.c v1.0

---
+++ 0x06-pointers_arrays_strings/100-print_number.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		if( n == -2147483648)
+		{
+			_putchar('2');
+			n = n % 1000000000;
+		}
+		n = -n;
+	}
+
+	if (n / 10 != 0)
+		print_number(n / 10);
+	_putchar('0' + (n % 10));
+}
