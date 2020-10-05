01331076d591485a9c6b299fd75c79f62381e24e
Selidex <justindparnell@yahoo.com>
2020-10-05 08:35:01 +0000

    4-strpbrk.c v1.0

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ *_strpbrk - finds if any set of chars are in a string
+ *@s: the string being searched
+ *@accept: the chars being searched for
+ *@Return: s if string found, null if not
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int i ,j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+				return (s + i);
+		}
+	}
+	return ('\0');
+}
