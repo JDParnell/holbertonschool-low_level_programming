accf9a8112c84c2478ad84eebe2e74f8ba5186f1
Selidex <justindparnell@yahoo.com>
2020-10-05 14:30:40 +0000

    9-set_string.c v1.0

---
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ *set_string - takes a pointer and sets it to a char
+ *@s: the string being set to the char
+ *@to: the char the string is being set too
+ *Return: void, no returns
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
