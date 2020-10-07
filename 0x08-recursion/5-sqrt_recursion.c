b42002f922212c995a6111bdadfaf47fa13f2229
Selidex <justindparnell@yahoo.com>
2020-10-07 20:06:11 +0000

    5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -29,13 +29,15 @@ int _sqrt_recursion(int n)
 {
 	int sqrt;
 
-	if (n <= 0)
+	if (n < 0)
 		return (-1);
+	else if(n == 0)
+		return (0);
 	else if (n == 1)
 		return (1);
 
 
-	sqrt = n / 2;
+	sqrt = n;
 	return (_do_sqrt(n, sqrt));
 
 
