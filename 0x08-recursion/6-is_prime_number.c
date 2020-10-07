22cb698c8e98af6296af93371c51d4e2e6b202e7
Selidex <justindparnell@yahoo.com>
2020-10-07 08:28:24 +0000

    6-is_prime_number.c v1.0

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+
+
+int _do_prime(int n, int prime)
+{
+	if (prime == 1)
+		return (1);
+	else if (n % prime == 0)
+		return (0);
+
+	return (_do_prime(n, prime - 2));
+}
+
+/**
+ *is_prime_number - determines if n is a prime number
+ *@n: the number we are priming
+ *Return: 1 if prime, 0 if not
+ */
+
+int is_prime_number(int n)
+{
+	int prime;
+
+	if (n <= 1 || (n > 2 && n % 2 == 0))
+		return (0);
+
+	prime = n - 2;
+
+	return (_do_prime(n, prime));
+}
