12d03caf643b399176507721a05901c04bf57d16
Selidex <justindparnell@yahoo.com>
2020-10-07 05:39:09 +0000

    0-puts_recursion.c v1.0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ *_puts_recursion - recursively prints each character of a string
+ *@s: the string being printed
+ *Return: void no return
+ */
+
+void _puts_recursion(char *s)
+{
+	_putchar(*s);
+	if (*(s + 1) != '\0')
+		_puts_recursion(s + 1);
+	if (*(s + 1) == '\0')
+		_putchar('\n');
+}
