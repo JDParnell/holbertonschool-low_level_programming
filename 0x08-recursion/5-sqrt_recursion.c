2d12ed11691ccf7a7c5627cdb0bb5b2f8f93548c
Selidex <justindparnell@yahoo.com>
2020-10-07 08:16:23 +0000

    5-sqrt_recursion.c v1.0

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,44 @@
+#include "holberton.h"
+
+
+/**
+ *_do_sqrt - finds the square root of n
+ *@n: the number being rooted
+ *@sqrt: the square root
+ *Return: the square root
+ */
+
+int _do_sqrt(int n, int sqrt)
+{
+	if (sqrt == 0)
+		return (-1);
+	else if ((n / sqrt) == sqrt && n % sqrt == 0)
+		return (sqrt);
+
+	return (_do_sqrt(n, sqrt - 1));
+}
+
+
+/**
+ *_sqrt_recursion - returns the square root of n
+ *@n: the number to be square rooted
+ *Return: the square root
+ */
+
+int _sqrt_recursion(int n)
+{
+	int sqrt;
+
+	if (n < 0)
+		return (-1);
+	else if (n == 0)
+		return (0);
+	else if (n == 1)
+		return (1);
+
+
+	sqrt = n / 2;
+	return (_do_sqrt(n, sqrt));
+
+
+}
