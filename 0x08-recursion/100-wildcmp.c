770592c180a14a066d5996deae06491ede6b84e3
Selidex <justindparnell@yahoo.com>
2020-10-08 04:15:37 +0000

    100-wildcmp.c v0.85

--- 0x08-recursion/100-wildcmp.c
+++ 0x08-recursion/100-wildcmp.c
@@ -27,4 +27,8 @@ int wildcmp(char *s1, char *s2)
 	if (*s1 == *s2)
 		return (wildcmp(s1 + 1, s2 + 1));
 
+	if (*s2 == '*' && *(s2 + 1) == *(s1 + 1))
+		return (wildcmp(s1 + 1, s2 + 1));
+
+	return (-1);
 }
