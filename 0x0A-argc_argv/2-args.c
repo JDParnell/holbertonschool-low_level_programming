42c1de260f30d591f19071fafb05b58f85fc9753
Selidex <justindparnell@yahoo.com>
2020-10-09 06:53:02 +0000

    2-args.c v1.0

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ *main - prints the argumetns passed to main
+ *@argc: number of arguments
+ *@argv: the array of arguments
+ *Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+		printf("%s\n", argv[i]);
+	return (0);
+}
