cbdbf4f8be4e9e55140937fa74ad75cd68af68e6
Selidex <justindparnell@yahoo.com>
2020-10-09 06:30:22 +0000

    0-whatsmyname.c v1.0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ *main - creates a program that prints the file name to the terminal
+ *@argv: array of commands passed to program
+ *@argc: number of commands
+ *Return: always success, returns 0
+ */
+
+int main(int argc, char *argv[])
+{
+	argc += 0;
+	printf("%s\n", argv[0]);
+	return (0);
+}
