c212fc158dadc617d49978350bf29dc69cf3ea4d
Selidex <justindparnell@yahoo.com>
2020-10-13 23:06:11 +0000

    5-argstostr.c v1.0

---
+++ 0x0B-malloc_free/5-argstostr.c
@@ -0,0 +1,58 @@
+#include "holberton.h"
+
+/**
+ *_strcat - adds one string to the end of another string
+ *@dest: the string getting added to
+ *@src: the string being added to
+ *Return: a pointer to the resulting string
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i, j;
+
+	for (i = 0; dest[i] != '\0'; i++)
+	{}
+
+	for (j = 0; src[j] != '\0'; j++)
+	{
+		dest[i] = src[j];
+		i++;
+	}
+	dest[i] = src[j];
+	return (dest);
+}
+
+/**
+ *argstostr - add all arguments from argv
+ *@ac: number of arguments
+ *@av: array of arguments
+ *Return: pointer to new line
+ */
+
+char *argstostr(int ac, char **av)
+{
+	int i, j, len = 0;
+	char *santa;
+	char *nl = "\n";
+
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[j] != '\0'; j++)
+		{
+			len++;
+		}
+		len++;
+	}
+
+	santa = malloc(sizeof(char) * (len + 1));
+
+	for (i = 0; i < ac; i++)
+	{
+		santa = _strcat(santa, av[i]);
+		santa = _strcat(santa, nl);
+	}
+	santa[len] = '\0';
+
+	return (santa);
+}
