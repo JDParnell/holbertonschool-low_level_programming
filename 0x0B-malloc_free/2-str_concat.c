ea2765a7fc37c5eb7f58c57483b44b1717683c29
Selidex <justindparnell@yahoo.com>
2020-10-13 20:16:28 +0000

    2-str_concat.c v1.2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -25,6 +25,9 @@ char *str_concat(char *s1, char *s2)
 
 	toothless = malloc((k + l + 1) * sizeof(char));
 
+	if (toothless == NULL)
+		return (NULL);
+
 	for (i = 0; s1[i] != '\0'; i++)
 		toothless[i] = s1[i];
 	for (j = 0; s2[j] != '\0'; j++)
