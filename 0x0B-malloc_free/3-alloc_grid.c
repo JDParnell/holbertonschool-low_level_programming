58ebe69bd918d47ff7c4009bdfa36fe43893dd99
Selidex <justindparnell@yahoo.com>
2020-10-13 21:08:05 +0000

    3-alloc_grid.c v1.0

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,40 @@
+#include "holberton.h"
+
+/**
+ *alloc_grid - creates a 2d array init to 0
+ *@width: width of grid
+ *@height: height of grid
+ *Return: pointer to the grid
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int **grid, i, j;
+
+	if (width <= 0)
+		return (NULL);
+	if (height <= 0)
+		return (NULL);
+
+	grid = malloc(height * sizeof(int *));
+
+	if (grid == NULL)
+		return (NULL);
+
+	for (i = 0; i < height; i++)
+	{
+		grid[i] = malloc(width * sizeof(int));
+		if (grid[i] == NULL)
+			return (NULL);
+	}
+
+	for (i = 0; i < height; i++)
+	{
+		for (j = 0; j < width; j++)
+		{
+			grid[i][j] = 0;
+		}
+	}
+
+	return (grid);
+}
