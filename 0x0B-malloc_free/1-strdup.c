6a8d454ac6c456a906c0b353ecb28fe2ea30479d
Selidex <justindparnell@yahoo.com>
2020-10-13 19:11:44 +0000

    1-strdup.c v1.0

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+
+/**
+ *_strdup - creates a copy of a string using malloc
+ *@str: the string to be copied
+ *Return: the pointer to the copy of the string
+ */
+
+char *_strdup(char *str)
+{
+	char *cpy;
+	int i;
+
+	if (str == NULL)
+		return (NULL);
+
+	cpy = malloc(sizeof(str) * sizeof(char));
+
+	if (cpy == NULL)
+		return (NULL);
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		cpy[i] = str[i];
+	}
+
+	return (cpy);
+
+}
