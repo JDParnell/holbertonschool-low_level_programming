1ffeca0d2260ae78f668a025927742aa6643a96b
Selidex <justindparnell@yahoo.com>
2020-10-15 18:23:19 +0000

    2-calloc.c v1.0

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -18,7 +18,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (a == NULL)
 		return (NULL);
 
-	for (i = 0; i < nmemb; i++)
-		a[i] = 0;
+	for (i = 0; i < (nmemb * size); i++)
+		a[i] = '\0';
 	return (a);
 }
