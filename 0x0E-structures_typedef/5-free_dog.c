e22d9749ae198e19c604b8a553d72e8a659c0be8
Dexter Selina Parnell <justindparnell@yahoo.com>
2020-10-19 05:02:40 -0500

    Delete 5-free_dog.c

--- 0x0E-structures_typedef/5-free_dog.c
+++
@@ -1,2 +0,0 @@
-Whenever creating a function to free memory you should always check to make sure what is passed to the function is not NULL
-to avoid attempting to free something that does not exist.
