1834ea7ef1d0a959a75b68272cf2d48a384884c8
Selidex <justindparnell@yahoo.com>
2020-10-19 09:38:41 +0000

    2-print_dog.c v1.0

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,29 @@
+#include "dog.h"
+#include <math.h>
+
+/**
+ *print_dog - prints a dogs info
+ *@d: the dog to be printed
+ *Return: void no return
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+		return;
+
+	if (d->name == NULL)
+		printf("Name: (nil)\n");
+	else
+		printf("Name: %s\n", d->name);
+
+	if (isnan(d->age))
+		printf("Age: (nil)\n");
+	else
+		printf("Age: %f\n", d->age);
+
+	if (d->owner == NULL)
+		printf("Owner: (nil)\n");
+	else
+		printf("Owner: %s\n", d->owner);
+}
