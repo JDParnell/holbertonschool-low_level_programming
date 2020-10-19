d5d503c03e6eb38daaaf3098f633d17f244560b4
Selidex <justindparnell@yahoo.com>
2020-10-19 18:46:35 +0000

    2-print_dog.c v1.1 removing math.h

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,5 +1,5 @@
 #include "dog.h"
-#include <math.h>
+
 
 /**
  *print_dog - prints a dogs info
@@ -17,10 +17,7 @@ void print_dog(struct dog *d)
 	else
 		printf("Name: %s\n", d->name);
 
-	if (isnan(d->age))
-		printf("Age: (nil)\n");
-	else
-		printf("Age: %f\n", d->age);
+	printf("Age: %f\n", d->age);
 
 	if (d->owner == NULL)
 		printf("Owner: (nil)\n");
