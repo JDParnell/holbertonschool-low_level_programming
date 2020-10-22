96b53c97c19ba9b700719f9cf8ab5a0e1b038593
Selidex <justindparnell@yahoo.com>
2020-10-22 00:42:15 +0000

    3-* v1.0

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,41 @@
+#include "3-calc.h"
+
+
+/**
+ *main - recieves commandline arguments and send them to math stuffs
+ *@argc: number of arguments
+ *@argv: the array of arguments
+ *Return: 0 on success, 98 if not 3 arguments, 99 if wrong operator, 100 if div
+ *or mod by 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int a, b;
+	char o = argv[2][0];
+
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	if (strlen(argv[2]) != 1 || (o != '+' && o != '-' && o != '*'	\
+				     && o != '/' && o != '%'))
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	if (b == 0 && (o == '/' || o == '%'))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	printf("%d\n", (*get_op_func(argv[2]))(a, b));
+	return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,60 @@
+#include "3-calc.h"
+
+/**
+ *op_add - does a + b
+ *@a: first int
+ *@b: second int
+ *Return: a + b
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ *op_sub - does a minus b
+ *@a: first int
+ *@b: second int
+ *Return: a - b
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ *op_mul - does a times b
+ *@a: first int
+ *@b: second int
+ *Return: a * b
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ *op_div - does a divided by b
+ *@a: first int
+ *@b: second int
+ *Return: a / b
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ *op_mod - does a mod b
+ *@a: first int
+ *@b: second int
+ *Return: a % b
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
