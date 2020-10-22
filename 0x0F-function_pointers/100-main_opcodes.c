a575849072f2ac8863c9eaa30921367257fb5fe2
Selidex <justindparnell@yahoo.com>
2020-10-22 01:41:17 +0000

    100-main_opcodes.c framework

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ *main - prints the opcodes of the main function
+ *@argc: number of arguments
+ *@argv: array of arguments
+ *Return: 0 on success, exits with 1 or 2 on failure
+ */
+
+int main(int argc, char *argv[])
+{
+	int bytes;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+
+	if (argv[1] < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	bytes = atoi(argv[1]);
+	return (0);
+
+}
