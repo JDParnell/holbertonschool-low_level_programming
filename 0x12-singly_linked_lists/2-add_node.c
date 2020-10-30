2b04b5c6a8e7fcc793dc9156831d2e4bfd02a55f
Selidex <justindparnell@yahoo.com>
2020-10-30 06:13:34 +0000

    2-add_node.c

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+
+/**
+ *add_node - adds a node to the beginning of a list
+ *@head: pointer to the head of the list
+ *@str: the string to be added
+ *Return: address of the new head
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new;
+	unsigned int i;
+
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
+	if (*head == NULL)
+		new->next = NULL;
+	else
+		new->next = *head;
+
+	new->str = strdup(str);
+
+	for (i = 0; new->str[i] != '\0'; i++)
+	{}
+	new->len = i;
+
+	*head = new;
+	return (*head);
+}
