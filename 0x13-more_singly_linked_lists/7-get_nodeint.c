d30a5d874021cce1cbe901f925f724df78cb688a
Selidex <justindparnell@yahoo.com>
2020-11-04 09:53:05 +0000

    7-get_nodeint.c

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ *get_nodeint_at_index - gets the nth node of a list
+ *@head: the list
+ *@index: the nth node
+ *Return: the nth node
+ */
+
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	listint_t *traverse = (listint_t *)head;
+	unsigned int i;
+
+	if (head == NULL)
+		return (NULL);
+
+	for (i = 0; i <= index; i++)
+	{
+		traverse = traverse->next;
+		if (traverse == NULL)
+			return (NULL);
+	}
+	return (traverse);
+}
