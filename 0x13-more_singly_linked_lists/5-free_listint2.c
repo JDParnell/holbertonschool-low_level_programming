eaba7714dc1f9505c63d11630fc0bd554d3a274f
Selidex <justindparnell@yahoo.com>
2020-11-04 09:39:32 +0000

    5-free_listint2.c

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ *free_listint2 - frees list then sets head to NULL
+ *@head: the list
+ *Return: void no return
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *freedom;
+
+	if (*head == NULL)
+		return;
+
+	while (*head != NULL)
+	{
+		freedom = *head;
+		*head = (*head)->next;
+		free(freedom);
+	}
+}
