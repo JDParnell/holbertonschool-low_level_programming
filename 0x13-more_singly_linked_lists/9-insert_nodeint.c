b565bf887deab2e4b80fdef08f258480cc6e8f15
Selidex <justindparnell@yahoo.com>
2020-11-04 10:41:28 +0000

    9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -16,7 +16,18 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (*head == NULL && idx != 0)
 		return (NULL);
 
-	for (i = 0; i < idx - 1; i++)
+	if (idx == 0)
+	{
+		new = malloc(sizeof(listint_t));
+		if (new == NULL)
+			return (NULL);
+		new->n = n;
+		new->next = *head;
+		*head = new;
+		return (new);
+	}
+
+	for (i = 0; i < idx; i++)
 	{
 		insert = insert->next;
 		if (insert == NULL && (i + 1) != idx)
