c65a8f556a16133a81ca065e7866c321b2164c3e
Selidex <justindparnell@yahoo.com>
2020-11-04 10:36:49 +0000

    9-insert_nodeint.c

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,35 @@
+#include "lists.h"
+
+/**
+ *insert_nodeint_at_index - inserts a node at index
+ *@head: the list
+ *@idx: where to insert
+ *@n: the data
+ *Return: the address of the new node, or NULL
+ */
+
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *new, *insert = (listint_t *) *head;
+	unsigned int i;
+
+	if (*head == NULL && idx != 0)
+		return (NULL);
+
+	for (i = 0; i < idx - 1; i++)
+	{
+		insert = insert->next;
+		if (insert == NULL && (i + 1) != idx)
+			return (NULL);
+	}
+
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+
+	new->n = n;
+	new->next = insert->next;
+	insert->next = new;
+
+	return (new);
+}
