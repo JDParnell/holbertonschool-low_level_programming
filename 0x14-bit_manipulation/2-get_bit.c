9563c236d3478de84ad86a81dca4270682e99564
Selidex <justindparnell@yahoo.com>
2020-11-06 02:14:52 +0000

    betty compliance

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,5 +1,12 @@
 #include "holberton.h"
 
+/**
+ *get_bit - retruns the bit at index
+ *@n: the number being tested
+ *@index: the bit to git
+ *Return: the bit at index, or -1
+ */
+
 int get_bit(unsigned long int n, unsigned int index)
 {
 	if (index >= 64)
