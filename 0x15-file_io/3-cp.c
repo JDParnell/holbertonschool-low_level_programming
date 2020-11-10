2a10c8cd57066ffcfb4aedb6076ad709160112af
Selidex <justindparnell@yahoo.com>
2020-11-10 05:47:23 +0000

    3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,6 +1,39 @@
 #include "holberton.h"
 
 /**
+ *printerror - prints an error message
+ *@err: the error code
+ *@filename: name of the file
+ *Return: the error code
+ */
+
+int printerror(int err, char *filename, int fc)
+{
+	if (err == 97)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		return (err);
+	}
+	else if (err == 98)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n",
+			filename);
+			return (err);
+	}
+	else if (err == 99)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", filename);
+		return (err);
+	}
+	else if (err == 100)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fc);
+		return (err);
+	}
+	return (0);
+}
+
+/**
  *main - mimics the shell cp command
  *@argc: number of commands given
  *@argv: array of commands given
@@ -9,44 +42,34 @@
 
 int main(int argc, char *argv[])
 {
-	int file_from, file_to;
+	int file_from, file_to, fft, ftt;
 	char buffer[1024];
 
 	if (argc != 3)
-	{		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);	}
+		exit(printerror(97, "NA", 0));
 	if (argv[1] == NULL)
-	{		dprintf(STDERR_FILENO,
-				"Error: Can't read from file %s\n", argv[1]);
-		exit(98);	}
+		exit(printerror(98, argv[1], 0));
 	file_from = open(argv[1], O_RDONLY);
 	if (file_from == -1)
-	{	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n"
-			, argv[1]);
-		exit(98);	}
+		exit(printerror(98, argv[1], 0));
 	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (file_to == -1)
-	{		dprintf(STDERR_FILENO,
-			"Error: Can't write to file %s\n", argv[2]);
-		exit(99);	}
+		exit(printerror(99, argv[2], 0));
+
 	while (read(file_from, buffer, 1024) && file_from != -1 &&
 	       write(file_to, buffer, 1024) && file_to != -1)
-	{}
-	if (file_from == -1)
-	{		dprintf(STDERR_FILENO,
-			"Error: Can't read from file %s\n", argv[1]);
-		exit(98); }
-	if (file_to == -1)
-	{		dprintf(STDERR_FILENO,
-			"Error: Can't write to file %s\n", argv[2]);
-		exit(99); }
-	file_from = close(file_from);
-	if (file_from == -1)
-	{		dprintf(STDERR_FILENO,
-			"Error: Can't close fd %d\n", file_from);
-		exit(100); }
-	if (close(file_to) == -1)
-	{		dprintf(STDERR_FILENO,
-				"Error: Can't close fd %d\n", file_to);
-		exit(100);	}
-	return (0); }
+	{
+		if (file_from < 0)
+			exit(printerror(98, argv[1], 0));
+		if (file_to < 0)
+			exit(printerror(99, argv[2], 0));
+	}
+
+	fft = close(file_from);
+	if (fft < 0)
+		exit(printerror(100, "NA", file_from));
+	ftt = close(file_to);
+	if (ftt < 0)
+		exit(printerror(100, "NA", file_to));
+	return (0);
+}
