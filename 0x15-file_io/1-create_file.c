5d03fc3a1f36db5652ed09286e1e3336fb6f3621
Selidex <justindparnell@yahoo.com>
2020-11-10 02:37:52 +0000

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,7 +15,7 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	fc = open(filename, O_CREAT | O_WRONLY, 6000);
+	fc = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 600);
 	if (fc == -1)
 		return (-1);
 
@@ -27,6 +27,8 @@ int create_file(const char *filename, char *text_content)
 	fc = write(fc, text_content, i);
 	if (fc == -1)
 		return (-1);
+
 	close(fc);
+
 	return (1);
 }
