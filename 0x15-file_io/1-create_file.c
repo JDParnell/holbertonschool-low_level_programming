7c463cae5e9dfe41ad2014034b64e780afb37eb8
Selidex <justindparnell@yahoo.com>
2020-11-10 02:25:21 +0000

    1-create_file.c

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+
+/**
+ *create_file - makes a file
+ *@filename: name of the file to be made
+ *@text_content: a null terminated string to write ot the file
+ *Return: 1 on success, -1 on failure
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	ssize_t fc;
+	int i;
+
+	if (filename == NULL)
+		return (-1);
+
+	fc = open(filename, O_CREAT | O_WRONLY, 6000);
+	if (fc == -1)
+		return (-1);
+
+	if (text_content == NULL)
+		text_content = "";
+
+	for (i = 0; text_content[i] != '\0'; i++)
+	{}
+	fc = write(fc, text_content, i);
+	if (fc == -1)
+		return (-1);
+	close(fc);
+	return (1);
+}
