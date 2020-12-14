5b9876c76f283fe90d6efe69b505f5369662b345
Selidex <justindparnell@yahoo.com>
2020-12-14 06:57:09 +0000

    2-add_dnodeint.c

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,23 @@
+#inlcude "lists.h"
+
+/**
+ *add_dnodeint - adds a new node at the beg of a list
+ *@head: the head of the list
+ *@n: the element to store in the new node
+ *Return: address of new node, or null if node creation fails
+ */
+
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new = malloc(sizeof(dlistint_t));
+
+	if (new == NULL)
+		return (NULL);
+
+	new->n = n;
+	new->prev = NULL;
+	(*head)->prev = new;
+	new->next = *head;
+	*head = new;
+	return (new);
+}
