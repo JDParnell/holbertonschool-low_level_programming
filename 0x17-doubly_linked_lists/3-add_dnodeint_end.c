2dc24ca9272dcca4617aebc249abe71023b87348
Selidex <justindparnell@yahoo.com>
2020-12-14 07:18:30 +0000

    3-add_dnodeint_end.c

---
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+
+/**
+ *add_dnodeint_end - adds a new node at the end of a list
+ *@head: the head of the list
+ *@n: the element to store in the new node
+ *Return: address of new node, or null if node creation fails
+ */
+
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlistint_t *new = malloc(sizeof(dlistint_t));
+
+	if (new == NULL)
+		return (NULL);
+
+	new->n = n;
+	new->next = NULL;
+	if (*head == NULL)
+	{
+		new->prev = NULL;
+		*head = new;
+	}
+	else
+	{
+		dlistint_t current = *head;
+		while(current->next != NULL)
+			current = current->next;
+		current->next = new;
+		new->prev = current;
+	}
+	return (new);
+}
