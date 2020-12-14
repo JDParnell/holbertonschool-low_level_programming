d05e445b2e0bfff5099d1879c8dc037548c51679
Selidex <justindparnell@yahoo.com>
2020-12-14 08:40:00 +0000

    7-insert_dnodeint.c

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -17,35 +17,36 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	if (new == NULL)
 		return (NULL);
-
 	if (find == NULL && idx != 0)
 		return (NULL);
 	new->n = n;
 	if (idx == 0)
-	{
-		new->next = *h;
+	{	new->next = *h;
 		new->prev = NULL;
 		if (*h != NULL)
 			(*h)->prev = new;
 		*h = new;
-		return (new);
-	}
+		return (new);	}
 	while (i < idx)
 	{
-		if (find->next == NULL && (i + 1) != idx)
-		{
-			return (NULL);
-		}
+		if (find->next == NULL && (i + 1) != index)
+		{ return (NULL); }
+		else if (find->next == NULL)
+		{ break; }
 		else
-		{
-			find = find->next;
-			i++;
-		}
+		{	find = find->next;
+			i++; }
 	}
-	tp = find->prev;
-	new->next = find;
-	tp->next = new;
-	find->prev = new;
-	new->prev = tp;
+	if (find->next == NULL)
+	{
+		new->next = NULL;
+		new->prev = find;
+		find->next = new;	}
+	else
+	{ tp = find->prev;
+		new->next = find;
+		tp->next = new;
+		find->prev = new;
+		new->prev = tp; }
 	return (new);
 }
