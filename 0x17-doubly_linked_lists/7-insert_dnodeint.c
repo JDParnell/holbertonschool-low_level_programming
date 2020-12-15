e82fbd83b1175f2d79c5caf37b273031e54a018d
Selidex <justindparnell@yahoo.com>
2020-12-15 01:49:31 +0000

    Added new functions

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -10,46 +10,39 @@
 
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *new = malloc(sizeof(dlistint_t));
+	dlistint_t *new;
 	dlistint_t *find;
 	unsigned int i = 0;
 	dlistint_t *tp;
 
+	if (idx == 0)
+	{	new = add_dnodeint(h, n);
+		return (new);	}
+
 	if (h == NULL)
 		return (NULL);
 	find = *h;
-	if (new == NULL)
-		return (NULL);
-	if (find == NULL && idx != 0)
-		return (NULL);
-	new->n = n;
-	if (idx == 0)
-	{	new->next = *h;
-		new->prev = NULL;
-		if (*h != NULL)
-			(*h)->prev = new;
-		*h = new;
-		return (new);	}
 	while (i < idx)
 	{
 		if (find->next == NULL && (i + 1) != idx)
 		{ return (NULL); }
 		else if (find->next == NULL)
-		{ break; }
+		{
+			new = add_dnodeint_end(h, n);
+			return (new);
+		}
 		else
 		{	find = find->next;
 			i++; }
 	}
-	if (find->next == NULL)
-	{
-		new->next = NULL;
-		new->prev = find;
-		find->next = new;	}
-	else
-	{ tp = find->prev;
-		new->next = find;
-		tp->next = new;
-		find->prev = new;
-		new->prev = tp; }
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
+	tp = find->prev;
+	new->next = find;
+	tp->next = new;
+	find->prev = new;
+	new->prev = tp;
 	return (new);
 }
