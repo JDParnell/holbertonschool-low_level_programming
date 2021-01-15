2a3fa8c42f49393ba10693ec706c6181509705c0
Selidex <justindparnell@yahoo.com>
2021-01-15 23:18:20 +0000

    0-hash_table_create.c

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,23 @@
+#include "hash_tables.h"
+
+/**
+ *hash_table_create - creates a hash table
+ *@size: the size of the table to create
+ *Return: table if created, NULL if error
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *new_hash;
+
+	if (size == 0)
+		return (NULL);
+
+	new_hash = malloc(sizeof(hash_table_t));
+	if (new_hash == NULL)
+		return (NULL);
+	new_hash->array = malloc(sizeof(hash_node_t) * size);
+	if (new_hash->array == NULL)
+		return (NULL);
+	return (new_hash);
+}
