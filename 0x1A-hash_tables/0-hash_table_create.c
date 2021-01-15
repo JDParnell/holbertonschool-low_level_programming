74349b5a7724fef11836dcf7505a809b0269e4de
Selidex <justindparnell@yahoo.com>
2021-01-15 23:35:17 +0000

    0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -11,13 +11,11 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *new_hash;
 	unsigned long int i;
 
-	if (size == 0)
-		return (NULL);
-
 	new_hash = malloc(sizeof(hash_table_t));
 	if (new_hash == NULL)
 		return (NULL);
 	new_hash->array = malloc(sizeof(hash_node_t) * size);
+	new_hash->size = size;
 	if (new_hash->array == NULL)
 	{
 		free(new_hash);
