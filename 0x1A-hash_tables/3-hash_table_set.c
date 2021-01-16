0f0ec92eda6bfd1ac2ee4b72f5d6efe05c2ccf0b
Selidex <justindparnell@yahoo.com>
2021-01-16 00:07:55 +0000

    3-hash_table_set.c

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,47 @@
+#include "hash_tables.h"
+
+/**
+ *hash_table_set - sets a hash table
+ *@ht: the hast table
+ *@key: the key to hash
+ *@value: the value of the item
+ *Return: 1 if success, 0 if fail
+ */
+
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int i;
+	hash_node_t *nn;
+	unsigned const char *nkey = (unsigned const char *)key;
+
+	nn = malloc(sizeof(hash_node_t));
+	if (!strcmp(key, ""))
+		return (0);
+	if (nn == NULL)
+		return (0);
+	nn->key = strdup(key);
+	if (nn->key == NULL)
+	{
+		free(nn);
+		return (0);
+	}
+	nn->value = strdup(value);
+	if (nn->value == NULL)
+	{
+		free(nn->key);
+		free(nn);
+		return (0);
+	}
+	i = key_index(nkey, ht->size);
+	if (ht->array[i] != NULL)
+	{
+		nn->next = NULL;
+		ht->array[i] = nn;
+	}
+	else
+	{
+		nn->next = ht->array[i];
+		ht->array[i] = nn;
+	}
+	return (1);
+}
