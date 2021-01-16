9aa72a49db0619574d0218c423f5b6fd4aba3943
Selidex <justindparnell@yahoo.com>
2021-01-15 19:15:00 -0600

    Burnt Earth Recovery

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,51 @@
+#include "hash_tables.h"
+
+/**
+ *hash_table_set - sets a hash table
+ *@ht: the hast table
+ *@key: the key to hash
+ *@value: the value of the item
+ *Return: 1 if success, 0 if fail
+ */
+
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int i;
+	hash_node_t *nn, *parser;
+
+	if (key == NULL || ht == NULL || value == NULL)
+		return (0);
+	if (strcmp(key, "") == 0)
+		return (0);
+	i = key_index((const unsigned char *)key, ht->size);
+	for (parser = ht->array[i]; parser != NULL; parser = parser->next)
+		{
+			if (strcmp(key, parser->key) == 0)
+			{
+				free(parser->value);
+				parser->value = strdup(value);
+				if (parser->value == NULL)
+					return (0);
+				return (1);
+			}
+		}
+	nn = malloc(sizeof(hash_node_t *));
+	if (nn == NULL)
+		return (0);
+	nn->key = strdup(key);
+	if (nn->key == NULL)
+	{	free(nn);
+		return (0); }
+	nn->value = strdup(value);
+	if (nn->value == NULL)
+	{	free(nn->key);
+		free(nn);
+		return (0); }
+	if (ht->array[i] == NULL)
+	{	nn->next = NULL;
+		ht->array[i] = nn; }
+	else
+	{	nn->next = ht->array[i]
+		ht->array[i] = nn; }
+	return (1);
+}
