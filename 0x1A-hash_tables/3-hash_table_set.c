a8b23ab5424baa67e01ccd6b4eb5b4b467345130
Selidex <justindparnell@yahoo.com>
2021-01-16 00:21:24 +0000

    3-hash_table_set.c

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -11,38 +11,40 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int i;
-	hash_node_t *nn;
+	hash_node_t *nn, *parser;
 
 	unsigned const char *nkey = (unsigned const char *)key;
 
-	nn = malloc(sizeof(hash_node_t));
 	if (!strcmp(key, ""))
 		return (0);
+	i = key_index(nkey, ht->size);
+	if (ht->array[i] != NULL)
+	{	parser = ht->array[i];
+		while (parser != NULL)
+		{
+			if (key == parser->key)
+			{	parser->value = strdup(value);
+				if (parser->value == NULL)
+					return (0);
+				return (1); } }	}
+	nn = malloc(sizeof(hash_node_t *));
 	if (nn == NULL)
 		return (0);
 	nn->key = strdup(key);
 	if (nn->key == NULL)
-	{
-		free(nn);
-		return (0);
-	}
+	{	free(nn);
+		return (0); }
 	nn->value = strdup(value);
 	if (nn->value == NULL)
-	{
-		free(nn->key);
+	{	free(nn->key);
 		free(nn);
-		return (0);
-	}
-	i = key_index(nkey, ht->size);
+		return (0); }
+
 	if (ht->array[i] != NULL)
-	{
-		nn->next = NULL;
-		ht->array[i] = nn;
-	}
+	{	nn->next = NULL;
+		ht->array[i] = nn; }
 	else
-	{
-		nn->next = ht->array[i];
-		ht->array[i] = nn;
-	}
+	{	nn->next = ht->array[i];
+		ht->array[i] = nn; }
 	return (1);
 }
