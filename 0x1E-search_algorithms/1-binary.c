c42a4d99c029ee956ad5e2d9e2973f0e110fd8d0
Selidex <justindparnell@yahoo.com>
2021-04-21 23:46:23 +0000

    test

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -3,36 +3,6 @@
 void print_array(int *array, size_t size);
 
 /**
- *binary_helper - like hamburger helper but for binary searches
- *@array: the array being searched
- *@size: size of the array
- *@value: the value being searched for
- *@oset: the offset value or the recursion to keep track of index
- *Return: -1 if not found, the index if found
- **/
-
-int binary_helper(int *array, size_t size, int value, int oset)
-{
-	size_t mid;
-
-	print_array(array, size);
-	if (size == 1)
-	{
-		if (array[oset] == value)
-			return (oset);
-		else
-			return (-1);
-	}
-	mid = (size / 2) + oset;
-	if (array[mid] == value)
-		return (mid);
-	if (array[mid] < value)
-		return (binary_helper(array, size / 2, value, mid));
-	else
-		return (binary_helper(array, size / 2, value, oset));
-}
-
-/**
  *binary_search - performs a binary search on an array
  *@array: the array being searched
  *@size: the size of the array
@@ -42,9 +12,22 @@ int binary_helper(int *array, size_t size, int value, int oset)
 
 int binary_search(int *array, size_t size, int value)
 {
+	size_t mid, l = 0, r = size - 1;
+
 	if (array == NULL)
 		return (-1);
-	return (binary_helper(array, size, value, 0));
+	while (l <= r)
+	{
+		print_array(array + l, r - l + 1);
+		mid = (r + l) / 2;
+		if (array[mid] == value)
+			return (mid);
+		if (array[mid] < value)
+			l = mid + 1;
+		else
+			r = mid - 1;
+	}
+	return (-1);
 }
 
 /**
